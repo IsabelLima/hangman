597584f75dbf54b6cc6f8334009006efe1a2f40f
Isabel <bellima93@gmail.com>
2021-03-14 18:17:33 -0300

    remove unused imports

--- src/main/java/com/isabel/hangman/HangmanApplication.java
+++ src/main/java/com/isabel/hangman/HangmanApplication.java
@@ -1,14 +1,10 @@
 package com.isabel.hangman;
 
-import com.isabel.hangman.utils.XmlLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.List;
-
 @SpringBootApplication
-@EnableJpaRepositories
 public class HangmanApplication {
 
 	public static void main(String[] args) {
